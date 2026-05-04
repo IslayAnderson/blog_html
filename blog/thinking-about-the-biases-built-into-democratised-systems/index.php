b92c60fbb8ed5a6acf197a2277a080d0bebf39ed
islayAnderson <me@islayanderson.co.uk>
2026-05-04 22:27:44 +0100

    new blog

---
+++ blog/thinking-about-the-biases-built-into-democratised-systems/index.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * "title":"Thinking about the biases built into "Democratised systems"",
+ * "excerpt":"'The System is fundamentally flawed', a phrase regularly used to describe the Academic journal system. Yet a pattern we see over and over again in democratised systems",
+ * "thumbnail":"thumbnail-5.jpg",
+ * "date":"2026-04-04"
+ */
+include $_SERVER['DOCUMENT_ROOT'] . '/assets/php/functions.php';
+head(get_post_meta(__FILE__));
+
+?>
+
+    <body>
+    <div class="basic">
+        <p>&ldquo;The System is fundamentally flawed&rdquo;, a phrase regularly used to describe the
+            Academic journal system. Throughout all history it seems that lying and deceiving in the pursuit of power
+            and so we create systems that give the illusion of democratisation but allow us to reinforce our own ideas
+            and biases; Think Parliament, The electoral college, Liberal democracy as a whole, and the Academic Journals
+            I&rsquo;m thinking about sit in this same space.<br/><br/></p>
+        <p>I&rsquo;m thinking about this because yesterday (03/04/26) I explored the way we use these systems to gain
+            influence and power over others.<br/>Medusa.is.a.heroin (instagram) is a relatively unknown yet persistent
+            blog dedicated to analytically engaging with TERF ideas and science, because of this I feel drawn to the
+            account despite their consistent use of pejorative language towards trans people, the account serves as a
+            useful repository of the current ideas and ideology of the intellectual TERF.</p>
+        <p>Yesterday (03/04/26) they shared a paper &ldquo;Transgender Homicides in Britain, 2000-2025 and Perpetrators"
+            by Michael Briggs of Oxford University. The paper tabulates reports of trans people as victims and
+            perpetrators of homicide with the intention of &ldquo;finding&rdquo; trends. Reasonable weekend science,
+            however the systems designed to highlight the biases built into the system not only highlight possible bad
+            science they act a beacon to those who wish to use the system for their own gain. In this example Professor
+            Briggs is a trustee of the &ldquo;charity&rdquo; Sex Matters on paper as of April 2024 but listed as a board
+            member on their website as early as October 2023. (A willingness to manipulate systems built for
+            accountability is implied)</p>
+        <p>Searching through Briggs Previous works something immediately stands out Previous to 2018 Briggs work was
+            incredibly broad but post 2018 Briggs begins to focus on sex and gender, his first work on the subject being
+            an article for the blog 4thWaveNow a blog created in 2015 in response to her trans sons transition which she
+            blames on social media. For those within the field 2018 stands out as a prominent date, 2018 being the year
+            the mainstream media latches onto a concept and field of study called &ldquo;Sudden onset Gender Identity
+            Disorder&rdquo; and suddenly it feels like the whole media institution wants to talk about this issue and
+            trans people as a topic.<br/><br/>What I'm alluding to is a phenomenon that's larger than this specific
+            topic, but within this topic arguably started with Dr Debra Soh.<br/>What happens is a scientist will be
+            investigating a controversial and/or politically influential field, their work will get picked up by a
+            politically motivated media outlet or thinktank and they will give them resources to pursue that field and
+            so begins the cycle of bad politically influenced science in one of two directions; continuing your studies
+            in the traditional academic setting working in university and taking funding or taking a position with a
+            media company and becoming a celebrity scientist.</p>
+        <p><br/><br/></p>
+    </div>
+    </body>
+
+<?php foot();
+
